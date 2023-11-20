b61b567e0d75565357b87997bee7f8caef0f6f75
Hasan BOZKUŞ <hhasanbozkus0147@gmail.com>
2023-11-20 12:55:57 +0300

    Add project files.

---
+++ Hubs/MyHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRSeverExample.Hubs
+{
+    public class MyHub : Hub
+    {
+        public async Task SendMessage(string message)
+        {
+            //Ekstra işlemler...
+            await Clients.All.SendAsync("receiveMessage", message);
+        }
+    }
+}
